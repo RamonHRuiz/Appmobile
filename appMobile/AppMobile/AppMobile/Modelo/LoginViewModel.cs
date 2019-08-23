442db2e8da7da459566819a9dd0493daa2021505
ramon ruiz pacheco <ramon.ruiz@repobox.mx>
2019-08-22 18:01:56 -0700

    Controladores para que funcione

---
+++ appMobile/AppMobile/AppMobile/Modelo/LoginViewModel.cs
@@ -0,0 +1,36 @@
+﻿namespace AppMobile.Modelo
+{
+    using System.Windows.Input;
+    public class LoginViewModel
+    {
+
+        #region Constructor
+        public LoginViewModel()
+        {
+            this.IsRemember = true;
+        }
+        #endregion
+
+        #region Propiedades
+        public string Email
+        { get; set; }
+
+        public string Password
+        { get; set; }
+
+        public bool IsRunning
+        { get; set; }
+
+        public bool IsRemember
+        { get; set; }
+        #endregion
+
+        #region Command
+        public ICommand LoginCommand
+        { get; set; }
+
+        public ICommand RegistCommand
+        { get; set; }
+        #endregion
+    }
+}
--- appMobile/AppMobile/AppMobile/Modelo/MainViewModel.cs
+++ appMobile/AppMobile/AppMobile/Modelo/MainViewModel.cs
@@ -2,5 +2,16 @@
 {
     public class MainViewModel
     {
+        #region ViewModels
+        public LoginViewModel Login
+        { get; set; }
+        #endregion
+
+        #region Constructor
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+        }
+        #endregion
     }
 }
